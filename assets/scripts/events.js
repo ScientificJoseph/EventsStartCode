c2f923ad54c7af786c7b394604869bf8660676c9
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-23 16:29:16 -0400

    used event propergation for delegation of events

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -46,13 +46,34 @@ form.addEventListener('submit', event => {
 const div = document.querySelector('div')
 
 div.addEventListener('click', event => {
+    button.style.backgroundColor = 'black'
+    // button.remove()
     console.log('Clicked Div')
     console.log(event);
 })
 
 button.addEventListener('click', event => {
+    // event.stopPropagation()
     console.log('Clicked Button')
     console.log(event)
+})
+
+const listItems = document.querySelectorAll('li')
+const list = document.querySelector('ul');
+
+// listItems.forEach((listItem) => {
+//     listItem.addEventListener('click', event =>{
+//         event.target.classList.toggle('highlight')
+//         // event.target.remove()
+//     })
+// })
+
+list.addEventListener('click', event =>{
+    // console.log(event.currentTarget)
+    // event.target.classList.toggle('highlight')
+    event.target.closest('li').classList.toggle('highlight')
+    // event.target.closest('li').remove()
+
 })
 
 

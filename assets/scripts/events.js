37a3031b3c9444257d818c5994083fa2fc1b8253
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-25 10:48:22 -0400

    final comments for section 14

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -50,7 +50,7 @@ div.addEventListener('click', event => {
     console.log(event);
 })
 
-button.addEventListener('click', function(event) { //traditional function changes the this context
+button.addEventListener('click', function(event) { //traditional function changes the this context to the node that fired the listner / must bind this if using arror functions
     event.stopPropagation()
     console.log('Clicked Button')
     console.log(event)
@@ -60,20 +60,20 @@ button.addEventListener('click', function(event) { //traditional function change
 const listItems = document.querySelectorAll('li')
 const list = document.querySelector('ul');
 
-// listItems.forEach((listItem) => {
+// listItems.forEach((listItem) => { // good for simple list item without ansector, sibling or child elements
 //     listItem.addEventListener('click', event =>{
 //         event.target.classList.toggle('highlight')
 //         // event.target.remove()
 //     })
 // })
 
-list.addEventListener('click', event =>{
+list.addEventListener('click', event =>{ // Event delegation by using direct ancestor and taking advantage of event propagation (bubbling/capturing). Good for simple list item with ansector, sibling or child elements
     // console.log(event.currentTarget)
     // event.target.classList.toggle('highlight')
     event.target.closest('li').classList.toggle('highlight')
     // form.submit()
     button.click()
-    // event.target.closest('li').remove()
+    event.target.closest('li').remove()
 
 })
 
